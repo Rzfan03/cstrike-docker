130446fc8c58e2362cb97da39840be0552c1fb5e
Zennius! <fajrulsyaban47@gmail.com>
2026-05-15 04:55:19 +0000

    SETUP MY OWN SERVER

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Setup ur Server..."
+sleep 2
+echo "Almost Done!"
+sleep 1
+docker run --name cstrike120 \
+  -p 27015:27015/udp \
+  -p 27015:27015 \
+  -v $(pwd)/files/server.cfg:/home/steam/cstrike/cstrike/server.cfg \
+  cajuclc/cstrike-docker
